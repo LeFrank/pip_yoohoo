27f7f136e2f1de6c2e3579219035f6988656b770
Francois Campbell <campbellfd@gmail.com>
2020-06-05 18:37:15 +0200

    First commt

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="yoohoo-FrancoisCampbell",
+	version="0.0.1",
+	author="Francois Campbell",
+	author_email="campbellfd@gmail.com",
+	description="Test Pip package",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://"
+)
